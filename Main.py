d1c2f6bbf6156b1051e533e1a60980f67caf1cf0
Muxen1 <max.anerud@gmail.com>
2025-11-20 13:06:40 +0100

    Simon - Skapade slutet av spelet
    La till fel gissningar och rätt gissningar och även en kod som säger om man redan gissat. La även till ett slut av spelet som antingen säger att man vann om man gissade rätt eller förlorade om man gissade fel. La även till starten av spelet och en loop för att gissa. Även antal försök i gissningarna

--- Main.py
+++ Main.py
@@ -42,3 +42,40 @@ def uppdatera_gissning(ordet, golvat_ord, bokstav):
             golvat_ord[i] = bokstav
             hittad = True
     return hittad
+
+def spela():
+    
+    #Huvudfunktion som styr spelet.
+    
+    ordet = slumpa_ord()
+    golvat_ord = skapa_ord(ordet)
+    antal_forsok = 7
+    gissade_bokstaver = []
+
+    # Spelloop
+    while antal_forsok > 0 and "_" in ord:
+        skriv_status(ord, antal_forsok)
+        bokstav = las_in_bokstav()
+
+        if bokstav in gissade_bokstaver:
+            print("Den bokstaven har du redan gissat!")
+            continue
+
+        gissade_bokstaver.append(bokstav)
+
+        if uppdatera_gissning(ordet, ord, bokstav):
+            print("Rätt gissat!")
+        else:
+            print("Fel gissning!")
+            antal_forsok -= 1
+
+    # Spelresultat
+    if "_" not in ord:
+        print(f"\nGrattis! Du vann ordet var: {ordet}")
+    else:
+        print(f"\nTyvärr, du förlorade! Ordet var: {ordet}")
+
+
+# Starta spelet
+if __name__ == "__main__":
+    spela()
